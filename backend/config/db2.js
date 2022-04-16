3f1f8a498bf29f24f3ba4ad51c7dd1ef761917be
Bolo821 <glorybolo@outlook.com>
2022-04-16 10:59:46 +0200

    test

--- backend/config/db2.js
+++ backend/config/db2.js
@@ -4,7 +4,7 @@ const connectDB = async () => {
     try {
         console.log("Hello welcome!");
        // const conn = await mongoose.connect('mongodb+srv://Bshal:bikalbishal@cluster0.wg4pf.mongodb.net/DukanList?retryWrites=true&w=majority')
-       const conn = await mongoose.connect('mongodb://localhost:27017/myFirstDatabase') 
+       const conn = await mongoose.connect('mongodb://localhost:27017/dukanlist') 
        console.log(`MongoDB Connected: ${ conn.connection.host }`.brightGreen)
     } catch(err) {
         console.log('error in connection to mongodh: ', err);        
